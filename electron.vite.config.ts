d4f5c58f90d2a1949d546fd0f4690bd7091cbf34
digua <i@digua.me>
2026-02-11 20:25:55 +0800

    chore: i18n构建配置

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,7 +2,6 @@ import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 import ui from '@nuxt/ui/vite'
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 export default defineConfig(() => {
   return {
@@ -47,14 +46,6 @@ export default defineConfig(() => {
               neutral: 'zinc',
             },
           },
-        }),
-        VueI18nPlugin({
-          // 指定全局语言包目录
-          include: [resolve(__dirname, 'src/i18n/locales/**')],
-          // 默认语言
-          defaultSFCLang: 'json',
-          // 启用组件内 <i18n> 块
-          compositionOnly: true,
         }),
       ],
       root: 'src/',
